cbd904f6feb7465d35d52b2ccaf19f00976cfdd0
Obi William <williamobi818@gmail.com>
2024-12-03 14:38:08 +0100

    port change

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import path from 'path';
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
